9e8db0e983749b1ef7b78832d8002a884013acea
Alecsandro Silva da Conceicao Junior <ascj@WINDOWS.CIN.UFPE.BR>
2017-11-16 15:24:40 -0300

    Adcionando a classe produto

--- src/Models/Produto.java
+++ src/Models/Produto.java
@@ -1,5 +1,35 @@
 package Models;
 
-public class Produto {
+public class produto {
+	private int codProd;
+	private String descricao;
+	private int valor;
+	
+	public int getcodProd(){
+		return this.codProd;
+	}
+	
+	public String getdescricao(){
+		return this.descricao;
+	}
+	
+	public int getvalor(){
+		return this.valor;
+	}
+	
+	public void setcodProd(int codProd){
+		this.codProd = codProd;
+	}
+	
+		public void setdescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	 
+	public void setvalor(int valor){
+		this.valor = valor;
+	}
 
+    public static void main(String[] args) {
+    	
+    }
 }
